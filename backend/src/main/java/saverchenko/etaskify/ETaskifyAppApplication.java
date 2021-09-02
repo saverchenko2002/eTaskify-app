37ee6cf009ad194f75dc90c997c6505832df9fd6
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-09-02 17:02:39 +0300

    add empty java project with dependecies

---
+++ backend/src/main/java/saverchenko/etaskify/ETaskifyAppApplication.java
@@ -0,0 +1,13 @@
+package saverchenko.etaskify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETaskifyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETaskifyAppApplication.class, args);
+    }
+
+}
